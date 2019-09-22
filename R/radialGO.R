b67d49efef83e764022fa811d9f251647483eec8
Bruno Pereira <brunocosper@gmail.com>
2019-09-22 17:38:00 -0400

    add initial files

---
+++ R/radialGO.R
@@ -0,0 +1,29 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+
+
+require(visNetwork, quietly = TRUE)
+require(GO.db, quietly = TRUE)
+
+# topGO enrichment analysis returns a list of GO IDs with a score (KS or Fischer)
+# need to grab nodes, organize with edges (relationships) and plot with visNetwork
+# TODO maybe use topGO's functions which already get the relationships
+# see: https://rdrr.io/bioc/topGO/src/R/topGOviz.R
+
+
+hello <- function() {
+  print("Hello, world!")
+}
