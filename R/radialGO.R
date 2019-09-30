4517d0880c142c982226a1084602cfc686238edb
Bruno Pereira <brunocosper@gmail.com>
2019-09-30 18:08:10 -0400

    update code to use DiagrammeR functions, use GO terms rather than IDs as node labels, remove unused code and comments, fix graph attributes

--- R/radialGO.R
+++ R/radialGO.R
@@ -1,94 +1,21 @@
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-# want to pass to top function (enrichment analysis results with scores, number of top nodes to use when determining graph)
-
+# TODO
+# change code to take in list of all nodes with scores, rather than just top x
+# color nodes according to scores
+# make nodes equidistant
+# add relationships edges (part_of, etc.)
+# cluster by cellular component
+# add documentation
 
 require(GO.db, quietly = TRUE)
 require(DataCombine, quietly = TRUE)
 require(igraph, quietly = TRUE)
 require(DiagrammeR, quietly = TRUE)
+require(searchable, quietly = TRUE)
 
 go_ids<-readRDS("example_ids.rds")
 
-# topGO enrichment analysis returns a list of GO IDs with a score (KS or Fischer)
-# need to grab nodes, organize with edges (relationships) and plot with visNetwork
-# TODO maybe use topGO's functions which already get the relationships
-# see: https://rdrr.io/bioc/topGO/src/R/topGOviz.R
-
-#get list of children for each node, so we can build edges
-
-
-get_edges <- function(ids){
-  edge_list<-data.frame(from=character(), to=character(), stringsAsFactors=FALSE)
-  for(id in ids){
-    children <- as.list(GOBPCHILDREN[[id]])
-    children <- children[!is.na(children)]
-    if(length(children) > 0){
-      in_common <- unique(intersect(children, ids))
-      for(i in in_common){
-        newrow <- data.frame(from=i, to=id)
-        edge_list <- InsertRow(edge_list, newrow)
-      }
-    }
-  }
-  return(edge_list)
-}
-
-get_nodes <- function(ids){
-  nodes <- data.frame(id=character(), stringsAsFactors = FALSE)
-  add_bp <- data.frame(id="GO:0008150") # add root BP term so it's the first one (root)
-  nodes <- InsertRow(nodes, add_bp)
-  for (i in ids){
-      newrow <- data.frame(id=i, stringsAsFactors = FALSE)
-      nodes <- InsertRow(nodes, newrow)
-      ancestors <- as.list(GOBPANCESTOR[[i]])
-      ancestors <- ancestors[!is.na(ancestors)]
-      ancestors <- ancestors[which(ancestors != "all")]
-      if(length(ancestors) > 0){
-        for(ancestor in ancestors){
-            newrow2 <- data.frame(id=ancestor, stringsAsFactors = FALSE)
-            nodes <- InsertRow(nodes, newrow2)
-        }
-      }
-  }
-  return(unique(nodes))
-}
-
-buildGraphvizGraph <- function(nodes, edges){
-  out <- "digraph Enrichment {
-          graph [layout=twopi, overlap = true, splines=curved, fontsize = 1, nodesep=10, ranksep=4]
-          node [shape=circle, fillcolor = orange, style = filled, width=2]
-          "
-  for(i in nodes){
-    title <- c("'", gsub(" ", "\n", Term(GOTERM[[as.character(i)]])), "'")
-    title <- paste(title, collapse="")
-    n <- paste(title, collapse='')
-    out <- paste(c(out, n, collapse="; "))
-  }
-  out <- paste(c(out, "edge [color=grey] "), collapse='')
-  for(i in 1:dim(edges)[1]){
-    fr <- c("'", gsub(" ", "\n", Term(GOTERM[[edges[i, ]["from"]]])), "'")
-    fr <- paste(fr, collapse='')
-    to <- c("'", gsub(" ", "\n", Term(GOTERM[[edges[i, ]["to"]]])), "'")
-    to <- paste(to, collapse='')
-    edg <- c(fr, to)
-    edg <- paste(edg, collapse=" -> ")
-    out <- paste(c(out, edg), collapse=" ")
-  }
-  out <- paste(out, "}", collapse="")
-  return(out)
-}
-
 getSubgraphNodes <- function(enrichment_results){
-  nodes<-data.frame(id=character(), stringsAsFactors = FALSE)
+  nodes <- data.frame(id=character(), stringsAsFactors = FALSE)
   for(i in enrichment_results[["id"]]){
     nodes <- rbind(nodes, data.frame(id=i))
     ancestors <- as.list(GOBPANCESTOR[[i]])
@@ -104,48 +31,84 @@ getSubgraphNodes <- function(enrichment_results){
   return(unique(nodes))
 }
 
-buildNodeDF <- function(enrichment_results){
-  nodes <- data.frame(nodes=character(), type=character(),
-                      label=character(), style=character(),
-                      color=character(), shape=character(),
-                      data=character())
-  nodeList <- getSubgraphNodes(enrichment_results)
+buildNodeDF <- function(enrichment_results, top){
+  n <- data.frame(nodes=character(), type=character(),
+                  label=character(), style=character(),
+                  color=character(), shape=character(),
+                  data=numeric(), stringsAsFactors = FALSE)
+  nodeList <- getSubgraphNodes(enrichment_results[1:top, ])
   names(nodeList) <- c("nodes")
-  nodes <- rbind(nodes, nodeList)
+  for(i in 1:dim(nodeList)){
+    if(as.character(nodeList[i, ]) %in% enrichment_results[["id"]]){
+      colorIndex <- (as.numeric(enrichment_results[enrichment_results$id == as.character(nodeList[i, ]), "score"])) * 100 + 1
+      nodeColor <- heat.colors(100)[colorIndex]
+      newNode <- data.frame(id=as.numeric(substr(as.character(nodeList[i, ]), 4, nchar(as.character(nodeList[i, ])))),
+                 type="normal",
+                 label= gsub(" ", "\n", Term(GOTERM[[as.character(nodeList[i, ])]])), style="filled",
+                 fillcolor=nodeColor,
+                 shape="circle", data=0,
+                 fontsize = 40, fontcolor = "black",
+                 stringsAsFactors = FALSE)
+    }
+    else{
+      newNode <- data.frame(id=as.numeric(substr(as.character(nodeList[i, ]), 4, nchar(as.character(nodeList[i, ])))),
+                            type="normal",
+                            label= gsub(" ", "\n", Term(GOTERM[[as.character(nodeList[i, ])]])), style="filled", fillcolor="white",
+                            shape="circle", data=0,
+                            fontsize = 40, fontcolor = "black",
+                            stringsAsFactors = FALSE)
+    }
+    n <- rbind(n, newNode)
+  }
+  #nodes <- rbind(nodes, nodeList)
   # TODO the above code is losing the empty columns, need to add default values for them
-  return(nodes)
+  return(n)
   # add node types, colour, etc here
 }
 
 
 
 buildEdgeDF <- function(nodeDF){
-  edges <- data.frame(from=character(), to=character(), rel=character())
-  for(i in nodeDF[["nodes"]]){
-    children <- as.list(GOBPCHILDREN[[i]])
+  edges <- data.frame(from=character(), to=character(), rel=character(), stringsAsFactors = FALSE)
+  terms <- invert(Term(GOTERM))
+  listOfNodes <- character()
+  for(i in nodeDF[["label"]]){
+    # build list of node GO IDs in the nodeDF here, to check with children
+    listOfNodes <- c(listOfNodes, terms[[gsub("\n", " ", i)]])
+  }
+  for(i in nodeDF[["label"]]){
+    go_id <- terms[[gsub("\n", " ", i)]]
+    children <- as.list(GOBPCHILDREN[[go_id]])
     children <- children[!is.na(children)]
     if(length(children) > 0){
-      in_common <- unique(intersect(children, nodeDF[["nodes"]]))
+      in_common <- unique(intersect(children, listOfNodes))
       for(j in in_common){
-        edges <- rbind(edges, data.frame(from=j, to=i, rel="leading_to"))
+        edges <- rbind(edges, data.frame(from=as.numeric(substr(j, 4, nchar(j))),
+                                         to=as.numeric(substr(go_id, 4, nchar(go_id))),
+                                         rel="leading_to"))
       }
     }
   }
   return(edges)
 }
 
-nodes <- buildNodeDF(go_ids)
+# need to pass in full go_ids with scores, and an integer (top x to use for subgraph generation)
+nodes <- buildNodeDF(go_ids, 10)
 edges <- buildEdgeDF(nodes)
-print(nodes)
-print(edges)
-gr <- create_graph(nodes_df = nodes, edges_df = edges, directed=TRUE)
+gr <- create_graph(nodes_df = nodes,
+                   edges_df = edges,
+                   directed=TRUE)
+gr <- add_global_graph_attrs(gr, attr="layout", value="twopi", attr_type = "graph")
+gr <- add_global_graph_attrs(gr, attr="root", value="8150", attr_type = "graph")
+gr <- add_global_graph_attrs(gr, attr="ranksep", value="10", attr_type = "graph")
+gr <- add_global_graph_attrs(gr, attr="nodesep", value="1.2", attr_type = "graph")
+gr <- add_global_graph_attrs(gr, attr="rank", value="max", attr_type = "graph")
+gr <- add_global_graph_attrs(gr, attr="smoothing", value="true", attr_type = "graph")
+gr <- add_global_graph_attrs(gr, attr="splines", value="curved", attr_type = "graph")
+gr <- add_global_graph_attrs(gr, attr="forcelabels", value="true", attr_type = "graph")
+gr <- add_global_graph_attrs(gr, attr="fixedsize", value="true", attr_type = "node")
+gr <- add_global_graph_attrs(gr, attr="width", value="5", attr_type = "node")
+gr <- add_global_graph_attrs(gr, attr="height", value="5", attr_type = "node")
+
+
 render_graph(gr)
-
-#DiagrammeR::grViz(gr)
-
-# TODO
-# change colour according to p-value or score
-# make nodes equidistant
-# add relationships edges (part_of, etc.)
-# cluster by cellular component
-# change buildgraph to use diagrammer
