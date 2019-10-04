47097347515025eaff7d06ef98562339b793e855
Bruno Pereira <brunocosper@gmail.com>
2019-10-04 01:41:54 -0400

    add functionality for reducing graph complexity by removing nodes below a user-provided cutoff

--- R/radialGO.R
+++ R/radialGO.R
@@ -1,18 +1,26 @@
 #' TODO
 #' Change color scale to better show the differences between small p-values
-#' Find some way to remove node overlap while keeping the radial shape intact
 #' Fix edge colors not showing (seems like a bug in DiagrammeR or Graphviz)
 #' Add documentation
 #' Fix naming conventions
 #' Cluster nodes by cellular component localization
-
+#' Add edge attributes to new edges after reducing
+#'
 require(GO.db, quietly = TRUE)
 require(DataCombine, quietly = TRUE)
 require(DiagrammeR, quietly = TRUE)
 require(searchable, quietly = TRUE)
 
-go_ids<-readRDS("example_ids.rds")
 
+#' Generate the subgraph implied by the list of nodes passed in.
+#' @param enrichment_results A named numerical vector containing GO IDs as names
+#'                           and the corresponding score
+#' @return A data frame containing one column, named "id", with the nodes needed
+#'         for building the graph
+#' @examples
+#' scores <- c(0.5, 0.2, 0.001)
+#' names(scores) <- c("GO:0008150", "GO:1901360", "GO:0006139")
+#' getSubgraphNodes(scores)
 getSubgraphNodes <- function(enrichment_results){
   nodes <- data.frame(id=character(), stringsAsFactors = FALSE)
   for (i in names(enrichment_results)){
@@ -30,26 +38,46 @@ getSubgraphNodes <- function(enrichment_results){
   return(unique(nodes))
 }
 
+#' Create a text label describing the GO term and its score
+#' @param goTerm A string containing the GO ID to use
+#' @param scores A named numerical vector containing GO IDs as names
+#'               and the corresponding score
+#' @return A string containing the label text
+#' @examples <- c(0.4)
+#' scores <- c(0.5, 0.2, 0.001)
+#' names(scores) <- c("GO:0008150", "GO:1901360", "GO:0006139")
+#' buildLabel(names(scores[1]), scores)
 buildLabel <- function(goTerm, scores){
   label <- paste(c(as.character(gsub(" ", "\n", Term(GOTERM[[goTerm]]))),
-                                "\n\n", as.character(round(scores[[goTerm]], 4)),
-                                "\n\n", goTerm),
+                                "\n\n", goTerm,
+                                "\n\n", as.character(round(scores[[goTerm]], 4))),
                  collapse='')
   return(label)
 }
 
+#' Builds a node data frame from the top GO terms using the enrichment results
+#' @param enrichment_results A named numerical vector containing GO IDs as names
+#'                           and the corresponding score
+#' @param top The number of top nodes to use to build the graph
+#' @examples
+#' scores <- c(0.5, 0.2, 0.001)
+#' names(scores) <- c("GO:0008150", "GO:1901360", "GO:0006139")
+#' buildNodeDF(scores, 2)
 buildNodeDF <- function(enrichment_results, top){
   n <- data.frame(nodes=character(), type=character(),
                   label=character(), style=character(),
                   color=character(), shape=character(),
                   data=numeric(), stringsAsFactors = FALSE)
   nodeList <- getSubgraphNodes(enrichment_results[1:top])
-  for (i in seq(along=dim(nodeList)[1])){
-    nodeLabel <- buildLabel(nodeList[i, ], go_ids)
+
+  for (i in seq(along=1:dim(nodeList)[1])){
+    nodeLabel <- buildLabel(nodeList[i, ], enrichment_results)
+
     if (as.character(nodeList[i, ]) %in% names(enrichment_results)){
       colorIndex <- (enrichment_results[nodeList[i, ]] *
                      length(enrichment_results))
-      nodeColor <- heat.colors(length(enrichment_results))[colorIndex]
+      nodeColor <- colorRampPalette(c("red", "black", "grey"),
+                                    space="rgb")(length(enrichment_results))[colorIndex]
       newNode <- data.frame(id=as.numeric(substr(nodeList[i, ], 4,
                                                  nchar(nodeList[i, ]))),
                             type="normal",
@@ -73,10 +101,21 @@ buildNodeDF <- function(enrichment_results, top){
   return(n)
 }
 
+#' Rebuild a GO ID string from its number
+#' @param goID A string or number containing the GO ID number
+#' @return The full GO ID
+#' @example rebuildGOID(8150)
+#'
 rebuildGOID <- function(goID){
   return(paste(c("GO:", rep("0",(7-nchar(goID))), goID), collapse=''))
 }
 
+#' Determine what color the edge should take in the graph.
+#' @param edgeRelationships A string containing a relationship between GO terms
+#' @return A string containing the color the edge should take
+#' @examples
+#' getEdgeColor("is_a")
+#'
 getEdgeColor <- function(edgeRelationships){
   edgeColor <- ""
   switch(edgeRelationships,
@@ -89,6 +128,18 @@ getEdgeColor <- function(edgeRelationships){
   return(edgeColor)
 }
 
+
+#' Build a data frame of edges from the nodes. NOTE: Due to a bug
+#' in the way DiagrammeR renders Graphviz objects, edges currently do not render
+#' with the correct stylings. To see that they are specified correctly, one can
+#' render the graph with the output="visNetwork" option, which shows correct
+#' edges but loses the node layout
+#' @param nodeDF A data frame containing at least columns id, label,
+#' @return A data frame of edges (from, to, rel)
+#' @examples
+#' scores <- c(0.5, 0.2, 0.001)
+#' names(scores) <- c("GO:0008150", "GO:1901360", "GO:0006139")
+#' buildEdgeDF(buildNodeDF(scores, 2))
 buildEdgeDF <- function(nodeDF){
   edges <- data.frame(from=character(), to=character(), rel=character(),
                       style=character(), stringsAsFactors = FALSE)
@@ -106,34 +157,78 @@ buildEdgeDF <- function(nodeDF){
     if (length(children) > 0){
       in_common <- unique(intersect(children, listOfNodes))
       for (j in in_common){
-        edgeStyle <- ""
         edgeColor <- ""
         lookUp <- searchable::invert(GOBPCHILDREN[[go_id]])
         edgeColor <- getEdgeColor(lookUp[[j]])
+        edgeLabel <- lookUp[[j]]
         edges <- rbind(edges, data.frame(from=as.numeric(substr(j, 4, nchar(j))),
                                          to=as.numeric(substr(go_id, 4,
                                                               nchar(go_id))),
                                          rel=lookUp[[j]],
                                          arrowsize=5,
-                                         color=edgeColor))
+                                         color=edgeColor,
+                                         headlabel=edgeLabel))
       }
     }
   }
   return(edges)
 }
 
-generateGraph <- function(scores){
-  nodes <- buildNodeDF(go_ids, 5)
-  edges <- buildEdgeDF(nodes)
-  gr <- create_graph(nodes_df = nodes,
-                     edges_df = edges,
-                     directed=TRUE)
 
+reduceGraph <- function(graph, cutoff){
+  edgeDF <- get_edge_df(graph)
+  print(edgeDF)
+  count <- 0
+  nodeDF <- get_node_df(graph)
+  for(i in nodeDF[["id"]]){
+    if(i == 8150){
+      # GO:0008150 is the biological_process node (root)
+      next
+    } else {
+      splitString <- strsplit(nodeDF[nodeDF$id == i, "label"], "\n")
+      score <- as.numeric(splitString[[1]][length(splitString[[1]])])
+      if(score > cutoff){
+        # we're deleting this node
+        edgesIn <- edgeDF[which(edgeDF$to == i), ]
+        edgesOut <- edgeDF[which(edgeDF$from == i), ]
+        if(nrow(edgesIn) != 0 && nrow(edgesOut) != 0){
+          for(j in seq(along=1:nrow(edgesIn))){
+            for(k in seq(along=1:nrow(edgesOut))){
+              newEdge <- data.frame(from = edgesIn[j, ]$from,
+                                    to = edgesOut[k, ]$to,
+                                    rel=edgesOut[k, ]$rel,  stringsAsFactors = FALSE)
+              edgeDF <- combine_edfs(edgeDF, newEdge)
+            }
+          }
+        }
+        #remove old edges
+        edgeDF <- edgeDF[edgeDF$to != i, ]
+        edgeDF <- edgeDF[edgeDF$from != i, ]
+        nodeDF <- nodeDF[nodeDF$id != i, ]
+        count <- count + 1
+      }
+    }
+    if(count == 1){print(edgeDF)}
+  }
+  return(create_graph(nodes_df = nodeDF, edges_df = edgeDF))
+}
+
+#' Generate the graph visualization of GO enrichment scores
+#' @param
+#' @return
+#' @examples
+generateGraph <- function(scores, top, cutoff){
+  nodes <- buildNodeDF(scores, top)
+  edges <- buildEdgeDF(nodes)
+
+
+  gr <- create_graph(nodes_df = nodes, edges_df = edges, directed=TRUE)
+  gr <- reduceGraph(gr, cutoff)
   gr <- add_global_graph_attrs(gr, attr="layout", value="twopi",
                                attr_type = "graph")
   gr <- add_global_graph_attrs(gr, attr="root", value="8150",
                                attr_type = "graph")
-  gr <- add_global_graph_attrs(gr, attr="ranksep", value="20",
+  gr <- add_global_graph_attrs(gr, attr="ranksep", value="15",
                                attr_type = "graph")
   gr <- add_global_graph_attrs(gr, attr="nodesep", value="20",
                                attr_type = "graph")
@@ -145,7 +240,7 @@ generateGraph <- function(scores){
                                attr_type = "graph")
   gr <- add_global_graph_attrs(gr, attr="orientation", value="[lL]*",
                                attr_type = "graph")
-  gr <- add_global_graph_attrs(gr, attr="overlap", value="prism0",
+  gr <- add_global_graph_attrs(gr, attr="overlap", value="true",
                                attr_type = "graph")
   gr <- add_global_graph_attrs(gr, attr="fixedsize", value="true",
                                attr_type = "node")
@@ -156,6 +251,10 @@ generateGraph <- function(scores){
   gr <- add_global_graph_attrs(gr, attr="penwidth", value="8",
                                attr_type = "edge")
 
-  render_graph(gr, width=1280, height=1024)
+  return(gr)
 }
+
+go_ids<-readRDS("example_ids.rds")
+graph <- generateGraph(go_ids, 5, 0.2)
+render_graph(graph, width=1280, height=1024)
 # [END]
